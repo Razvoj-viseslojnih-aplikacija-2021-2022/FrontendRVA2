43b22ab6857f060bee2d2f25b6ce939ddfd032a8
jelenapetrovacki <jpetrovacki3@gmail.com>
2022-04-27 14:20:47 +0200

    Vežbe 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VoziloComponent } from './components/primer-components/vozilo/vozilo.component';
+import { AutomobilComponent } from './components/primer-components/automobil/automobil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoziloComponent,
+    AutomobilComponent
   ],
   imports: [
     BrowserModule
