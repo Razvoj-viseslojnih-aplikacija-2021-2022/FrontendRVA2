796a58fdbe47ee28ed000955104706bc77486d88
jelenapetrovacki <jpetrovacki3@gmail.com>
2022-05-30 12:59:46 +0200

    Promena endpoint-a

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
-export const ARTIKL_URL = 'http://localhost:8083/artikl';
-export const DOBAVLJAC_URL = 'http://localhost:8083/dobavljac';
-export const PORUDZBINA_URL = 'http://localhost:8083/porudzbina';
-export const STAVKA_PORUDZBINE_URL = 'http://localhost:8083/stavkaPorudzbine';
-export const STAVKA_PORUDZBINE_ZAPORUDZBINU_URL = 'http://localhost:8083/stavkeZaPorudzbinaID';
+export const ARTIKL_URL = 'https://grupa2-backendrva.herokuapp.com/artikl';
+export const DOBAVLJAC_URL = 'https://grupa2-backendrva.herokuapp.com/dobavljac';
+export const PORUDZBINA_URL = 'https://grupa2-backendrva.herokuapp.com/porudzbina';
+export const STAVKA_PORUDZBINE_URL = 'https://grupa2-backendrva.herokuapp.com/stavkaPorudzbine';
+export const STAVKA_PORUDZBINE_ZAPORUDZBINU_URL = 'https://grupa2-backendrva.herokuapp.com/stavkeZaPorudzbinaID';
